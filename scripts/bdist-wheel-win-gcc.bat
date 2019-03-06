e08599991e770625db8049ac08561c30f6342c03
Pasi Miettinen <pmiettinen@exafore.com>
2019-03-06 14:24:53 +0200

    Deprecate py3.4 support

--- scripts/bdist-wheel-win-gcc.bat
+++ scripts/bdist-wheel-win-gcc.bat
@@ -9,7 +9,7 @@ CALL conda create -y -n py%1 python=%1 pip
 CALL conda activate py%1
 pip install virtualenvwrapper-win
 CALL rmvirtualenv venv
-CALL mkvirtualenv -r requirements-win-cp27-cp34.txt venv
+CALL mkvirtualenv -r requirements-win-cp27.txt venv
 md build
 cd build
 cmake  -D SKIP_UNIT_TESTS=true .. -G "MinGW Makefiles"
