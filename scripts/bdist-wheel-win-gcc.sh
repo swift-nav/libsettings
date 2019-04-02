fe73901542d2aa94b6f02569fb4f842ce35332eb
Pasi Miettinen <pmiettinen@exafore.com>
2019-04-02 01:34:33 -0700

    Fix travis win build sanity check

--- scripts/bdist-wheel-win-gcc.sh
+++ scripts/bdist-wheel-win-gcc.sh
@@ -4,7 +4,5 @@ if [ -z "$1" ]
 fi
 # For MinGW make to work correctly sh.exe must NOT be in your path.
 mv "/c/Program Files/Git/usr/bin/sh.exe" "/c/Program Files/Git/usr/bin/hidden_sh.exe"
-./scripts/bdist-wheel-win-gcc.bat $1
+./scripts/bdist-wheel-win-gcc.bat $1 || exit 1
 mv "/c/Program Files/Git/usr/bin/hidden_sh.exe" "/c/Program Files/Git/usr/bin/sh.exe"
-conda create -y -n py$1 python=$1 pip
-source activate py$1
--- scripts/bdist-wheel-win-msvc.sh
+++ scripts/bdist-wheel-win-msvc.sh
@@ -2,6 +2,4 @@ if [ -z "$1" ]
   then
     exit 1
 fi
-./scripts/bdist-wheel-win-msvc.bat $1
-conda create -y -n py$1 python=$1 pip
-source activate py$1
+./scripts/bdist-wheel-win-msvc.bat $1 || exit 1
