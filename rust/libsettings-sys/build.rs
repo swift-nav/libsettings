842c64b5d2a183b24c65fabeb0e1784c82ef6da2
Steve Meyer <steve.meyer@swift-nav.com>
2021-09-10 16:34:15 -0700

    test build

--- rust/libsettings-sys/build.rs
+++ rust/libsettings-sys/build.rs
@@ -1,27 +1,18 @@
 fn main() {
-    let dst = cmake::Config::new("../..")
+    cmake::Config::new("../..")
         .define("SKIP_UNIT_TESTS", "ON")
         .define("BUILD_SHARED_LIBS", "OFF")
         .define("libsettings_ENABLE_PYTHON", "OFF")
         .build();
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
 
     let dst = cmake::Config::new(".").build();
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-
     println!("cargo:rustc-link-lib=static=sbp");
     println!("cargo:rustc-link-lib=static=settings");
-    println!("cargo:rustc-link-lib=static=rustbindsettings");
 
     let bindings = bindgen::Builder::default()
         .header("./libsettings_wrapper.h")
         .clang_arg("-I../../include")
-        .clang_arg("-I../../third_party/libsbp/c/include")
-        .blocklist_item("FP_INFINITE")
-        .blocklist_item("FP_NAN")
-        .blocklist_item("FP_ZERO")
-        .blocklist_item("FP_SUBNORMAL")
-        .blocklist_item("FP_NORMAL")
         .generate()
         .expect("Unable to generate bindings");
 
