4c2aeeb092dea4a4aead694098c0c58f9b3d6d11
Steve Meyer <steve.meyer@swift-nav.com>
2021-09-11 22:13:26 -0700

    windows

--- rust/libsettings-sys/build.rs
+++ rust/libsettings-sys/build.rs
@@ -1,18 +1,24 @@
 fn main() {
-    cmake::Config::new("../..")
+    let dst = cmake::Config::new("../..")
         .define("SKIP_UNIT_TESTS", "ON")
         .define("BUILD_SHARED_LIBS", "OFF")
         .define("libsettings_ENABLE_PYTHON", "OFF")
         .build();
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
 
     let dst = cmake::Config::new(".").build();
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
+
     println!("cargo:rustc-link-lib=static=sbp");
     println!("cargo:rustc-link-lib=static=settings");
+    println!("cargo:rustc-link-lib=static=swiftnav");
+    println!("cargo:rustc-link-lib=static=rustbindsettings");
 
     let bindings = bindgen::Builder::default()
         .header("./libsettings_wrapper.h")
         .clang_arg("-I../../include")
+        .clang_arg("-I../../third_party/libswiftnav/include")
+        .clang_arg("-I../../third_party/libsbp/c/include")
         .generate()
         .expect("Unable to generate bindings");
 
