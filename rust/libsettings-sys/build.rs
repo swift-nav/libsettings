7131904c5c708c89433ecee577e7ac63c57ac54c
Steve Meyer <steve.meyer@swift-nav.com>
2021-09-11 22:21:04 -0700

    blocklist

--- rust/libsettings-sys/build.rs
+++ rust/libsettings-sys/build.rs
@@ -16,6 +16,7 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
         .header("./libsettings_wrapper.h")
+        .blocklist_item("FP__.*")
         .clang_arg("-I../../include")
         .clang_arg("-I../../third_party/libswiftnav/include")
         .clang_arg("-I../../third_party/libsbp/c/include")
